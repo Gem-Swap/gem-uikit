008fd691e30a5a06be8fe816a53a8c8b682f1445
Casweeney Ojukwu <casweeno2000@gmail.com>
2022-01-07 00:27:07 +0100

    Color modification

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,7 +2,7 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#F72B50",
-  primary: "#ee8c1f",
+  primary: "#842ABF",
   primaryBright: "#e66000",
   primaryDark: "#d95b00",
   secondary: "#7645D9",
@@ -26,7 +26,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#452A7A",
   textDisabled: "#BDC2C4",
-  textSubtle: "#ee8c1f",
+  textSubtle: "#842ABF",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -42,13 +42,13 @@ export const darkColors: Colors = {
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#0a0e17",
-  input: "#483f3b",
+  input: "#241B2F",
   inputBorder: "#cccccc",
   primaryDark: "#d95b00",
   tertiary: "#2d2f37",
   text: "#FFFFFF",
   textDisabled: "#666171",
-  textSubtle: "#ee8c1f",
+  textSubtle: "#842ABF",
   borderColor: "#524B63",
   card: "#000",
   gradients: {
